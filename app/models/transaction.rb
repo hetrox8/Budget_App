68c7e1de47322aaea9476bfc720a49c2064b7330
suleiman-gacheru <dretrevor8@gmail.com>
2023-11-23 16:04:07 +0300

    configure transactions

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,8 +1,9 @@
 class Transaction < ApplicationRecord
   belongs_to :author, class_name: 'User'
-  has_many :transaction_categories, dependent: :destroy
+  has_many :transaction_categories, foreign_key: :transaction_record_id
   has_many :categories, through: :transaction_categories
 
   validates :name, presence: true
   validates :amount, presence: true
 end
+
